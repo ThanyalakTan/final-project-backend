57b67b433e4366e3f212705feaf00afa23c0a847
เอกการัณภูมิ ตันเยี่ยนนิติ <thanyalak15601@gmail.com>
2023-10-24 23:58:08 +0700

    edit HandleError

--- app.js
+++ app.js
@@ -31,54 +31,89 @@ app.post('/signup', async (req, res) => {
   try {
     if(firstname && lastname && email && password && rePassword && height && weight && age && birthdate && gender) {
       if(password !== rePassword) {
-        res.status(400).json(`Passwords did not match`);
+        return res.status(400).json(`Passwords did not match`);
       }
       const findUser = await UserModel.findOne({ email });
       if(findUser){
         return res.status(400).json('Sorry, email already exits')
       }
       const userData = await UserModel.create({ firstname, lastname, email, password:bcrypt.hashSync(password, salt), height, weight, age, birthdate, gender })
-      console.log(userData);
       return res.status(200).json(userData);
       } else {
-        res.status(400).json(`Please fill empty form`);
+        return res.status(400).json(`Please fill the empty form`);
       }
   } catch (error) {
     return res.status(500).json({error: error.message})
   }
 })
+
 // Create route Login when click login
 app.post('/login', async (req, res) => {
   const { email, password } = req.body;
-  if(email && password) {
-    const findUserLogin = await UserModel.findOne({ email })
-    if(!findUserLogin) {
-      return res.status(404).json('Email or password incorrect')
-    }
-    const comparePassword = bcrypt.compareSync(password, findUserLogin.password);
-    if(comparePassword) {
-      jwt.sign({findUserLogin, id: findUserLogin._id}, process.env.mySecret, {}, (err, token) => {
-        return res.status(200).json({token});
-      })
-    } else {
-        return res.status(400).json('Email or password incorrect')
+  try {
+    if(email && password) {
+      const findUserLogin = await UserModel.findOne({ email })
+      if(!findUserLogin) {
+        return res.status(404).json('Email or password incorrect')
       }
-  } else {
-    return res.status(400).json(`Please input your email or password`)
+      const comparePassword = bcrypt.compareSync(password, findUserLogin.password);
+      if(comparePassword) {
+        jwt.sign({findUserLogin, id: findUserLogin._id}, process.env.mySecret, {}, (err, token) => {
+          return res.status(200).json({token});
+        })
+      } else {
+          return res.status(400).json('Email or password incorrect')
+        }
+    } else {
+      return res.status(400).json(`Please input your email or password`)
+    }
+  } catch (error) {
+    return res.status(500).json({error: error.message})
   }
 })
 
 // Create route when click Create Activities
 app.post('/activity', verifyAuth, async (req, res) => {
   const { activityType, activityName, description, duration, distance, date } = req.body;
-  console.log('description=>', description)
   const userId = req.userId;
   if (activityType && activityName && duration && date) {
     const userActivity = await ActivityModel.create({ activityType, activityName, description, duration, distance, date, userId });
-    console.log(userActivity)
-    res.status(200).json(userActivity);
+    return res.status(200).json(userActivity);
   } else {
-    res.status(400).json('Please fill the required form: Activity Type, Activity Name, Duration, and Date');
+    return res.status(400).json('Please fill the required form: Activity Type, Activity Name, Duration, and Date');
+  }
+})
+
+app.put('/profile', verifyAuth, async (req, res) => {
+  const userId = req.userId;
+  const { firstname, lastname, height, weight, age } = req.body;   // get value from body
+  try {
+    if( firstname && lastname && height && weight && age ) {
+      const updateProfile = await UserModel.findByIdAndUpdate(userId, { firstname, lastname, height, weight, age }, {new:true})
+      return res.status(200).json(updateProfile);
+    } else {
+      return res.status(400).json('Please fill the empty form')
+    }
+  } catch (error) {
+    return res.status(500).json({error: error.message})
+  }
+})
+
+// update activities
+app.put('/activity/:id', verifyAuth, async (req, res) => {
+  const { activityName, description, duration, distance } = req.body;
+  const userId = req.userId;
+  const activityId = req.params.id;
+  try {
+    if( activityName && duration ) {
+      const updateActivity = await ActivityModel.findOneAndUpdate({ _id: activityId, userId: userId }, 
+      { activityName, description, duration, distance }, {new: true});
+      return res.status(200).json(updateActivity);
+    } else {
+      return res.status(400).json('Please fill the required form: ActivityName and Duration');
+    }
+  } catch (error) {
+    return res.status(500).json({error: error.message})
   }
 })
 
@@ -86,26 +121,15 @@ app.post('/activity', verifyAuth, async (req, res) => {
 app.post('/goal', verifyAuth, async (req, res) => {
   const { activityType, activityName, duration, distance, deadline, status } = req.body;
   const userId = req.userId;
-  if(activityType && activityName && duration && deadline) {
-    const userGoal = await GoalModel.create({ activityType, activityName, duration, distance, deadline, userId, status });
-    res.status(200).json(userGoal);
-  } else {
-    res.status(400).json('Please fill the require form: Activity Type, Activity Name, Duration, and Deadline');
-  }
-})
-
-// update activities
-app.put('/activity/:id', verifyAuth, async (req, res) => {
-  const { activityName, description, duration, distance } = req.body;
-  console.log(activityName, description, duration, distance)
-  const userId = req.userId;
-  const activityId = req.params.id;
-  if( activityName && duration ) {
-    const updateActivity = await ActivityModel.findOneAndUpdate({ _id: activityId, userId: userId }, 
-    { activityName, description, duration, distance }, {new: true});
-    res.status(200).json(updateActivity);
-  } else {
-    res.status(400).json('Please fill the required form: Activity Type, Activity Name, Duration, and Date');
+  try {
+    if(activityType && activityName && duration && deadline) {
+      const userGoal = await GoalModel.create({ activityType, activityName, duration, distance, deadline, userId, status });
+      return res.status(200).json(userGoal);
+    } else {
+      return res.status(400).json('Please fill the require form: Activity Type, Activity Name, Duration, and Deadline');
+    }
+  } catch (error) {
+    return res.status(500).json({error: error.message})
   }
 })
 
@@ -114,27 +138,41 @@ app.put('/goal/:id', verifyAuth, async (req, res) => {
   const { activityType, activityName, duration, distance, deadline, status } = req.body;
   const userId = req.userId;
   const goalId = req.params.id;
-  if( activityType && activityName && duration && deadline ) {
-    const updateGoal = await GoalModel.findOneAndUpdate({ _id: goalId, userId: userId}, { activityType, activityName, duration, distance, deadline, status }, {new: true});
-    res.status(200).json(updateGoal);
-  } else {
-    res.status(400).json('Please fill the require form: Activity Type, Activity Name, Duration, and Deadline');
+  try {
+    if( activityType && activityName && duration && deadline ) {
+      const updateGoal = await GoalModel.findOneAndUpdate({ _id: goalId, userId: userId}, { activityType, activityName, duration, distance, deadline, status }, {new: true});
+      return res.status(200).json(updateGoal);
+    } else {
+      return res.status(400).json('Please fill the require form: Activity Type, Activity Name, Duration, and Deadline');
+    }
+  } catch (error) {
+    return res.status(500).json({error: error.message})
   }
+
 })
 
 // read(get) the activities
 app.get('/activity', verifyAuth, async (req, res) => {
   const userId = req.userId;
-  console.log(req.userId)
-  const findActivity = await ActivityModel.find({userId})
-  res.status(200).json(findActivity);
+  try {
+    const findActivity = await ActivityModel.find({userId})
+    return res.status(200).json(findActivity);
+  } catch (error) {
+    return res.status(500).json({error: error.message})
+  }
+
 })
 
 // read(get) goals
 app.get('/goal', verifyAuth, async (req, res) => {
   const userId = req.userId;
-  const findGoal = await GoalModel.find({userId})
-  res.status(200).json(findGoal);
+  try {
+    const findGoal = await GoalModel.find({userId})
+    return res.status(200).json(findGoal);
+  } catch (error) {
+    return res.status(500).json({error: error.message})
+  }
+
 })
 
 // delete activity
@@ -143,9 +181,9 @@ app.delete('/activity/:id', verifyAuth, async (req, res) => {
     const userId = req.userId;
     const activityId = req.params.id;
     const deleteActivity = await ActivityModel.findByIdAndRemove({ _id: activityId, userId: userId });
-    res.status(200).json(deleteActivity);
+    return res.status(200).json(deleteActivity);
   } catch (error) {
-    console.log(error)
+    return res.status(500).json({error: error.message})
   }
 })
 
@@ -155,9 +193,9 @@ app.delete('/goal/:id', verifyAuth, async (req, res) => {
     const userId = req.userId;
     const goalId = req.params.id;
     const deleteGoal = await GoalModel.findByIdAndRemove({ _id: goalId, userId: userId });
-    res.status(200).json(deleteGoal);
+    return res.status(200).json(deleteGoal);
   } catch (error) {
-    console.log(error)
+    return res.status(500).json({error: error.message})
   }
 })
 
@@ -165,18 +203,10 @@ app.get('/profile', verifyAuth, async (req, res) => {
   const userId = req.userId;
   try {
     const getUser = await UserModel.findById(userId);
-    res.status(200).json(getUser);
+    return res.status(200).json(getUser);
   } catch (error) {
-    console.log(error)
+    return res.status(500).json({error: error.message})
   }
-})
-
-app.put('/profile', verifyAuth, async (req, res) => {
-  const userId = req.userId;
-  const { firstname, lastname, height, weight, age } = req.body;   // get value from body
-  console.log(firstname)
-  const updateProfile = await UserModel.findByIdAndUpdate(userId, { firstname, lastname, height, weight, age }, {new:true})
-  res.status(200).json(updateProfile);
 })
 
 // Run on port 3000
